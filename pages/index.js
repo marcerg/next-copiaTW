61d229a152d2dd57610f3aed7e5211c69ef4cda0
Marcelo Rodriguez Gaete <marcelo20022010@hotmail.com>
2023-08-08 14:39:13 -0300

    pagina login

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
 import Head from 'next/head'
-import Link from 'next/link'
-import {useRouter} from 'next/router'
 import AppLayout from '../components/AppLayout'
-// devit
+import { colors } from '../style/theme'
+import Button from '../components/Button'
+import GitHub from '../components/Icons/GitHub'
 
 export default function Home() {
-  const router = useRouter()
-
   return (
     <>
       <Head>
@@ -15,11 +13,51 @@ export default function Home() {
       </Head>
 
       <AppLayout>
-        <h1>
-          <a href="https://nextjs.org">devter</a>
-        </h1>
+        <section>
+          <img src='/devter-logo.png' alt='Logo'/>
+          <h1>Devter</h1>
+          <h2>Talk about development<br />with developers 👨‍💻👩‍💻</h2>
+          <div>
+            <Button>
+              <GitHub fill='#fff' width={22} height={22}/>
+              Login with GitHub
+            </Button>
+          </div>
+        </section>
 
       </AppLayout>
+
+      <style jsx>{`
+        img {
+          width: 120px;
+        }
+
+        section {
+          display: grid;
+          height: 100%;
+          place-content: center;
+          place-items: center;
+        }
+
+        h1 {
+          color: ${colors.secondary};
+          font-weight: 800;
+          margin-bottom: 16px;
+        }
+
+        h2 {
+          color: ${colors.primary};
+          font-size: 17px;
+          margin: 0;
+        }
+
+        div {
+          margin-top: 8px;
+        }
+
+        `}
+
+      </style>
     </>
   )
 }
