98a3b2ca44f371dea97618c2108e3a1ffa4651ef
Marcelo Rodriguez Gaete <marcelo20022010@hotmail.com>
2023-08-16 11:58:16 -0300

    usando Firebase para crear y recuperar tweets

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,28 @@
 import Head from "next/head";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import AppLayout from "../components/AppLayout";
 import { colors } from "../style/theme";
 import Button from "../components/Button";
 import GitHub from "../components/Icons/GitHub";
-import Avatar from "../components/Avatar";
 import Logo from "../components/Icons/Logo";
+import useUser, { USER_STATES } from "../hooks/useUser";
 
-import { loginWithGitHub, onAuthStateChanged } from "../firebase/client";
+import { useRouter } from "next/router";
+
+import { loginWithGitHub } from "../firebase/client";
 
 export default function Home() {
-  const [user, setUser] = useState(undefined);
+  const user = useUser;
+  const router = useRouter();
 
   useEffect(() => {
-    onAuthStateChanged(setUser);
-  }, []);
+    user && router.replace("/home");
+  }, [user]);
 
   const handleClick = () => {
-    loginWithGitHub()
-      .then(setUser)
-      .catch((err) => {
-        console.log(err);
-      });
+    loginWithGitHub().catch((err) => {
+      console.log(err);
+    });
   };
 
   return (
@@ -41,21 +42,13 @@ export default function Home() {
             with developers 👨‍💻👩‍💻
           </h2>
           <div>
-            {user === null && (
+            {user === USER_STATES.NOT_LOGGED && (
               <Button onClick={handleClick}>
                 <GitHub fill="#fff" width={24} height={24} />
                 Login with GitHub
               </Button>
             )}
-            {user && user.avatar && (
-              <div>
-                <Avatar
-                  alt={user.username}
-                  src={user.avatar}
-                  text={user.username}
-                />
-              </div>
-            )}
+            {user === USER_STATES.NOT_KNOWN && <img src="/spinner.gif" />}
           </div>
         </section>
       </AppLayout>
